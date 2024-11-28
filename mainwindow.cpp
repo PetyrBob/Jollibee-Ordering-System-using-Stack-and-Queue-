f81c28326b0234cebd7757961ea1723d363ae9b5
Peter Bob Domogma <domogmapeterbob@gmail.com>
2024-11-29 00:10:53 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,17 +10,17 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Initialize the timer
+
     timer = new QTimer(this);
     connect(timer, &QTimer::timeout, this, &MainWindow::updateTimer);
-    timerDuration = 3 * 60;  // 3 minutes in seconds
+    timerDuration = 3 * 60;  
     timeLeft = timerDuration;
 
-    // LCD number initialization
-    ui->lcdNumber->setDigitCount(5); // Format: mm:ss
+  
+    ui->lcdNumber->setDigitCount(5); 
     updateLCD(timeLeft);
 
-    // Connect buttons to slots
+   
     connect(ui->Order, &QPushButton::clicked, this, &MainWindow::addOrder);
     connect(ui->Queue, &QPushButton::clicked, this, &MainWindow::startQueue);
     connect(ui->Serve, &QPushButton::clicked, this, &MainWindow::serveOrder);
@@ -48,16 +48,16 @@ void MainWindow::updateTimer()
         updateLCD(timeLeft);
     } else {
         timer->stop();
-        // Move the first order from pending_orders to completed_orders
+        
         if (ui->pending_orders->count() > 0) {
             QListWidgetItem* item = ui->pending_orders->takeItem(0);
             ui->completed_orders->addItem(item);
-            // Update the count of completed orders
+            
             updateCompletedOrdersCount();
-            // Reset the timer
+          
             timeLeft = timerDuration;
             updateLCD(timeLeft);
-            // Start the timer again if there are more pending orders
+          
             if (ui->pending_orders->count() > 0) {
                 timer->start(1000);
             }
@@ -69,16 +69,16 @@ void MainWindow::addOrder()
 {
     QString orderText = ui->lineEdit->text();
     if (!orderText.isEmpty()) {
-        // Generate an order number
+      
         QString orderNumber = QString::asprintf("#%03d", ui->pending_orders->count() + 1);
 
-        // Format the order with the order number
+
         QString orderWithNumber = orderNumber + ": " + orderText;
 
-        // Add the order to the pending orders list
+       
         ui->pending_orders->addItem(orderWithNumber);
 
-        // Clear the input field
+       
         ui->lineEdit->clear();
     }
 }
@@ -86,24 +86,24 @@ void MainWindow::addOrder()
 void MainWindow::startQueue()
 {
     if (!timer->isActive() && ui->pending_orders->count() > 0) {
-        timeLeft = timerDuration;  // Reset the timer to 3 minutes
+        timeLeft = timerDuration;  
         updateLCD(timeLeft);
-        timer->start(1000);  // Start the timer with 1-second intervals
+        timer->start(1000); 
     }
 }
 
 void MainWindow::serveOrder()
 {
-    // Move the first order from pending_orders to completed_orders
+   
     if (ui->pending_orders->count() > 0) {
         QListWidgetItem* item = ui->pending_orders->takeItem(0);
         ui->completed_orders->addItem(item);
-        // Update the count of completed orders
+        
         updateCompletedOrdersCount();
-        // Reset the timer
+        
         timeLeft = timerDuration;
         updateLCD(timeLeft);
-        // Start the timer again if there are more pending orders
+       
         if (ui->pending_orders->count() > 0) {
             timer->start(1000);
         } else {
@@ -114,20 +114,20 @@ void MainWindow::serveOrder()
 
 void MainWindow::deleteOrder()
 {
-    // Delete selected order from pending_orders
+   
     QListWidgetItem* item = ui->pending_orders->currentItem();
     if (item) {
         delete item;
     }
 }
 
-// New method to update the number of completed orders
+
 void MainWindow::updateCompletedOrdersCount()
 {
     int completedOrderCount = ui->completed_orders->count();
     QString countText = QString("Completed Orders: %1").arg(completedOrderCount);
     ui->completed_orders_2->clear();
-    ui->completed_orders_2->addItem(countText);  // Update the completed orders counter
+    ui->completed_orders_2->addItem(countText); 
 }
 
 void MainWindow::updatePendingOrdersCount()
@@ -135,7 +135,7 @@ void MainWindow::updatePendingOrdersCount()
     int pendingOrderCount = ui->pending_orders->count();
     QString countText = QString("Pending Orders: %1").arg(pendingOrderCount);
     ui->completed_orders_3->clear();
-    ui->completed_orders_3->addItem(countText);  // Update the completed orders counter
+    ui->completed_orders_3->addItem(countText);  
 }
 
 
